ae99d99458886ff0b68e7a724a5a865a2f9cac73
Baryo <or.bar.yosef@gmail.com>
2018-05-30 14:41:54 +0300

    add support for dyn evaluated script src

--- src/installers/frontend/ScriptTagInstaller.ts
+++ src/installers/frontend/ScriptTagInstaller.ts
@@ -6,13 +6,26 @@ export interface IScript {
     attributes?: { [name: string]: string };
 }
 
-export type ScriptGeneratorFn = (req, config) => Promise<IScript | string>
+export type ScriptGeneratorFn = (req, config) => Promise<IScript | string>;
+
+export interface ScriptNode extends CodeNode {
+    src: string;
+}
+
+const attributePrefix = 'attr-';
 
 export class ScriptTagInstaller extends DomInstaller {
-    protected run(fn: ScriptGeneratorFn, options, req) {
+    protected run(fn: ScriptGeneratorFn, options: ScriptNode, req) {
         return super.run(async (req, config) => {
+            const runtimeOptions: ScriptNode = Object.assign({}, options);
+            if (options.src) {
+                // can evaluate options.src - req & config can be used.
+                const scriptSrc = options.src.startsWith('`') ? eval(options.src) : options.src;
+                runtimeOptions[`${attributePrefix}src`] = scriptSrc;
+            }
+
             const scriptData = await fn(req, config);
-            const {code, attributes} = this.getScript(scriptData, options);
+            const {code, attributes} = this.getScript(scriptData, runtimeOptions);
 
             return `
 <script ${Object.keys(attributes).map(key => `${key}="${attributes[key]}"`).join(' ')}>
@@ -22,7 +35,7 @@ ${code}
         }, options, req);
     }
 
-    private getScript(scriptData: IScript | string, options: CodeNode) {
+    private getScript(scriptData: IScript | string, options: ScriptNode) {
         let script: IScript;
         if (typeof scriptData == 'object') {
             script = scriptData;
@@ -31,11 +44,19 @@ ${code}
         }
 
         const nodeScriptAttrs = Object.keys(options)
-            .filter(opt => opt.startsWith('attr-'))
+            .filter(opt => opt.startsWith(attributePrefix))
             .reduce((res, attr) => {
-                res[attr.replace('attr-', '')] = options[attr];
+                res[attr.replace(attributePrefix, '')] = options[attr];
                 return res;
             }, {});
+
+        if (options.src) {
+            // if (options.src.startsWith('`') ) {
+            // }
+            // else {
+                nodeScriptAttrs['src'] = options.src;
+            // }
+        }
 
         script.attributes = Object.assign(script.attributes || {}, nodeScriptAttrs);
         script.code = script.code || '';
