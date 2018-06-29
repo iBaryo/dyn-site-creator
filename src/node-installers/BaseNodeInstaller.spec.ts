f587670c6cfd608ea234df4a95e4a722a67c9bc5
baryo <baryo.stuff@gmail.com>
2018-06-29 13:34:56 +0300

    BaseNodeInstaller tests

--- src/node-installers/BaseNodeInstaller.spec.ts
+++ src/node-installers/BaseNodeInstaller.spec.ts
@@ -1,28 +1,39 @@
-// import {BaseNodeInstaller} from "./BaseNodeInstaller";
-// import {ICodeActivator, ICodeComponent} from "../code-components/interfaces";
-// import {CodeNode} from "../ConfigurationTypes";
-//
-// describe('BaseNodeInstaller', () => {
-//     class MockNodeInstaller extends BaseNodeInstaller<ICodeComponent<any>> {
-//         public install(node : CodeNode) : ICodeActivator<any> { return null; }
-//     }
-//
-//     let installer : BaseNodeInstaller<ICodeComponent<any>>;
-//     let mockCmpMap : Map<string, ICodeComponent<any>>;
-//     const mockCmpType = 'mock-type';
-//     beforeEach(() => {
-//         mockCmpMap = new Map<string, ICodeComponent<any>>();
-//         mockCmpMap.set(mockCmpType, {});
-//         installer = new MockNodeInstaller(mockCmpMap);
-//     });
-//
-//     describe('getComponent', () => {
-//         it('should return component according to type', () => {
-//
-//         });
-//         it('should return component according to object\'s type', () => {
-//         });
-//         it('should return undefined if no component with that name', () => {
-//         });
-//     });
-// });
+import {BaseNodeInstaller} from "./BaseNodeInstaller";
+import {ICodeActivator, ICodeComponent, ICodeComponentType, IContext} from "../code-components/interfaces";
+import {CodeNode} from "../ConfigurationTypes";
+
+describe('BaseNodeInstaller', () => {
+    class MockNodeInstaller extends BaseNodeInstaller<ICodeComponent<any>> {
+        public install(node : CodeNode) : ICodeActivator<any> { return null; }
+    }
+
+    let installer : BaseNodeInstaller<ICodeComponent<any>>;
+    let mockCmpMap : Map<string, ICodeComponent<any>>;
+    const mockCmpTypeName = 'mock-type';
+    let mockCmp: ICodeComponent<any>;
+    beforeEach(() => {
+        mockCmpMap = new Map<string, ICodeComponent<any>>();
+        mockCmp = {install: jasmine.createSpy()};
+        mockCmpMap.set(mockCmpTypeName, mockCmp);
+        installer = new MockNodeInstaller(mockCmpMap);
+    });
+
+    describe('getComponent', () => {
+        it('should return component according to type', () => {
+            expect(installer.getComponent(mockCmpTypeName)).toBe(mockCmp);
+        });
+        it('should return component according to object\'s type', () => {
+            class MockComponentType implements ICodeComponent<any>{
+                constructor(context: IContext) {
+                }
+                public static typeName = mockCmpTypeName;
+                public install() {}
+            }
+
+            expect(installer.getComponent<ICodeComponent<any>>(MockComponentType)).toBe(mockCmp);
+        });
+        it('should return undefined if no component with that name', () => {
+            expect(installer.getComponent('non-existing')).toBeUndefined();
+        });
+    });
+});
